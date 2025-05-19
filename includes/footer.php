11d54a149a13900f785a089adb486eb9ceda206d
Vedha Varshini <152283972+vedha-varshini@users.noreply.github.com>
2025-05-19 14:19:46 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,180 @@
+ <!--
+      - FOOTER
+    -->
+  
+    <footer>
+  
+  <div class="footer-category">
+
+    <div class="container">
+
+      <h2 class="footer-category-title">Brand directory</h2>
+
+      <div class="footer-category-box">
+
+        <h3 class="category-box-title">Fashion :</h3>
+
+        <a href="../mens/casual_men.php" class="footer-category-link">T-shirt</a>
+        <a href="../mens/formals_men.php" class="footer-category-link">Shirts</a>
+        <a href="../mens/denims.php" class="footer-category-link">shorts & jeans</a>
+        <a href=".,/mens/jacket.php" class="footer-category-link">jacket</a>
+        <a href="./casual_women.php" class="footer-category-link">dress & frock</a>
+
+
+      </div>
+
+      <div class="footer-category-box">
+        <h3 class="category-box-title">footwear :</h3>
+      
+        <a href="#" class="footer-category-link">sport</a>
+        <a href="#" class="footer-category-link">formal</a>
+        <a href="#" class="footer-category-link">Boots</a>
+        <a href="#" class="footer-category-link">casual</a>
+        <a href="#" class="footer-category-link">cowboy shoes</a>
+        <a href="#" class="footer-category-link">safety shoes</a>
+        <a href="#" class="footer-category-link">Party wear shoes</a>
+        <a href="#" class="footer-category-link">Branded</a>
+        <a href="#" class="footer-category-link">Firstcopy</a>
+        <a href="#" class="footer-category-link">Long shoes</a>
+      </div>
+    </div>
+</div>
+<div class="footer-nav">
+
+    <div class="container">
+
+      <ul class="footer-nav-list">
+
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Popular Categories</h2>
+        </li>
+
+        <li class="footer-nav-item">
+          <a href="../mens/casual_men.php" class="footer-nav-link">MEN'S</a>
+        </li>
+
+        <li class="footer-nav-item">
+          <a href="./casual_women.php" class="footer-nav-link">Women's</a>
+        </li>
+
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Shoes</a>
+        </li>
+      </ul>
+
+      <ul class="footer-nav-list">
+      
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Products</h2>
+        </li>
+      
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Prices drop</a>
+        </li>
+      
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">New products</a>
+        </li>
+      
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Best sales</a>
+        </li>
+       </ul>
+
+      <ul class="footer-nav-list">
+      
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Our Company</h2>
+        </li>
+
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Delivery</a>
+        </li>
+        <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Terms and conditions</a>
+        </li>
+       <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">About us</a>
+        </li>
+      </ul>
+      <ul class="footer-nav-list">
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Services</h2>
+        </li>
+       <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Delivery</a>
+        </li>
+       <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Return Policy</a>
+        </li>
+       <li class="footer-nav-item">
+          <a href="#" class="footer-nav-link">Contact us</a>
+        </li>
+      </ul>
+      <ul class="footer-nav-list">
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Contact</h2>
+        </li>
+         <li class="footer-nav-item flex">
+          <div class="icon-box">
+            <ion-icon name="location-outline"></ion-icon>
+          </div>
+           <address class="content">
+            419 Kumarswamy Layout 
+           Bengaluru, 560078, Karnataka
+          </address>
+        </li>
+        <li class="footer-nav-item flex">
+          <div class="icon-box">
+            <ion-icon name="call-outline"></ion-icon>
+          </div>
+          <a href="tel:+607936-8058" class="footer-nav-link">(+91)9354678901</a>
+        </li>
+        <li class="footer-nav-item flex">
+          <div class="icon-box">
+            <ion-icon name="mail-outline"></ion-icon>
+          </div>
+          <a href="mailto:example@gmail.com" class="footer-nav-link">node@gmail.com</a>
+        </li>
+      </ul>
+      <ul class="footer-nav-list">
+        <li class="footer-nav-item">
+          <h2 class="nav-title">Follow Us</h2>
+        </li>
+        <li>
+          <ul class="social-link">
+            <li class="footer-nav-item">
+              <a href="#" class="footer-nav-link">
+                <ion-icon name="logo-facebook"></ion-icon>
+              </a>
+            </li>
+            <li class="footer-nav-item">
+              <a href="#" class="footer-nav-link">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </a>
+            </li>
+            <li class="footer-nav-item">
+              <a href="#" class="footer-nav-link">
+                <ion-icon name="logo-linkedin"></ion-icon>
+              </a>
+            </li>
+            <li class="footer-nav-item">
+              <a href="#" class="footer-nav-link">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <img src="../assets/images/payment.png" alt="payment method" class="payment-img">
+      <p class="copyright">
+        &copy;Copyright <a href="#">Node8</a> all rights reserved.
+      </p>
+    </div>
+  </div>
+
+</footer>
